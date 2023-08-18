2d94021a671e3701fb46bdfc7ef62a735c230141
Chinnakorn <chinnakorn.parkot@gmail.com>
2023-08-18 16:26:39 +0700

    add plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -81,6 +81,19 @@ M.setup = function()
     },
     {
       "fatih/vim-go"
+    },
+    {
+      "sindrets/diffview.nvim",
+      event = "BufRead",
+    },
+    {
+      "folke/todo-comments.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
     }
   }
 end
